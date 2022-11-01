40fea014ecc4182da3b5c9f140c051d4941ae37d
Stefan-Tosev <115991937+Stefan-Tosev@users.noreply.github.com>
2022-11-01 13:39:57 +0200

    Update Example1.java

--- Example1.java
+++ Example1.java
@@ -4,14 +4,18 @@ public class Example1 {
     public static void main(String[] args) {
         int myInt = 9;
         double myDouble = 20.7;
-int y= 25;
-        System.out.println("числото е integer " + myInt);
-        System.out.println("числото е double " + myDouble);
-         if   (y>myInt && y<myDouble)
-        System.out.println("числото е между двете числа");
-         else  {
 
-        System.out.println("числото не е между двете числа");
+        Scanner scan = new Scanner(System.in); // creating a scanner object
+        System.out.print("Enter number  " );
+
+        int num = scan.nextInt(); // reading the number
+        //System.out.print("The number is " + num);
+
+        if ((num<myInt)||(num>myDouble)) {
+            System.out.println("the number isnt between the two numbers"); // block of code to be executed if the condition is true
+        } else { System.out.println("the number is between the two numbers"); 
+        
+        
     }
 
 }
